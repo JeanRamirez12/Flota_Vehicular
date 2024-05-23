6d534bb69a7292f46734a6afe26bec2fdd8e819c
JeanRamirez12 <fzafrc@gmail.com>
2024-05-23 16:27:50 -0500

    Create Asignaciones

---
+++ Pages/Asignaciones/Index.cshtml.cs
@@ -0,0 +1,25 @@
+using Flota_Vehicular.Data;
+using Flota_Vehicular.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flota_Vehicular.Pages.Asignaciones
+{
+    public class IndexModel : PageModel
+    {
+		private readonly FlotavehicularContext _context;
+		public IndexModel(FlotavehicularContext context)
+		{
+			_context = context;
+		}
+		public IList<Asignacion> Asignaciones { get; set; }
+		public async Task OnGetAsync()
+		{
+			//if (_context.Categories != null)
+			//{
+			Asignaciones = await _context.Asignaciones.ToListAsync();
+			//}
+		}
+	}
+}
