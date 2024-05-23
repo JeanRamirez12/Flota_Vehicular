be5a825eb48e7585af08be500becac1343ae1c0c
JeanRamirez12 <fzafrc@gmail.com>
2024-05-23 16:08:18 -0500

    Delete Vehiculos

--- Pages/Vehiculos/Delete.cshtml.cs
+++ Pages/Vehiculos/Delete.cshtml.cs
@@ -1,12 +1,57 @@
+using Flota_Vehicular.Data;
+using Flota_Vehicular.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace Flota_Vehicular.Pages.Vehiculos
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly FlotavehicularContext _context;
+        public DeleteModel(FlotavehicularContext context)
         {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Vehiculo Vehiculos { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehiculo = await _context.Vehiculos.FirstOrDefaultAsync(m => m.Id == id);
+
+
+            if (vehiculo == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vehiculo = await _context.Vehiculos.FindAsync(id);
+
+            if (vehiculo != null)
+            {
+                Vehiculos = vehiculo;
+                _context.Vehiculos.Remove(Vehiculos);
+                await _context.SaveChangesAsync();
+
+            }
+
+            return RedirectToPage("./Index");
         }
     }
 }
