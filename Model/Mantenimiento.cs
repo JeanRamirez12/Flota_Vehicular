73cc2aa60dbd82aec7ad94eb2287a392b14d85fb
Jhorman montoya <jhormaparaelcreador@gmail.com>
2024-05-23 14:59:11 -0500

    Creacion de Data y Model

---
+++ Model/Mantenimiento.cs
@@ -0,0 +1,11 @@
+﻿namespace Flota_Vehicular.Model
+{
+    public class Mantenimiento
+    {
+        public int Id { get; set; }
+        public DateTime FechaMantenimiento { get; set; }
+        public string Descripcion { get; set; }
+        public int Costo { get; set; }
+
+    }
+}
