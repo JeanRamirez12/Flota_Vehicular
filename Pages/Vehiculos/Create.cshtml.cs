2d9992a4a28fdbe7676b2112c956fdc0494245ce
JeanRamirez12 <fzafrc@gmail.com>
2024-05-23 16:00:18 -0500

    Create Vehiculos

--- Pages/Vehiculos/Create.cshtml.cs
+++ Pages/Vehiculos/Create.cshtml.cs
@@ -1,3 +1,5 @@
+using Flota_Vehicular.Data;
+using Flota_Vehicular.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,31 @@ namespace Flota_Vehicular.Pages.Vehiculos
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
+        private readonly FlotavehicularContext _context;
+        public CreateModel(FlotavehicularContext context)
         {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Vehiculo Vehiculos { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Vehiculos == null || Vehiculos == null)
+            {
+                return Page();
+            }
+
+            _context.Vehiculos.Add(Vehiculos);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
         }
     }
 }
