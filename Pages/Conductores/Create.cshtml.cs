57d7aacbb4ce65cf93faaa547ea1b62162166868
Jhorman montoya <jhormaparaelcreador@gmail.com>
2024-05-23 16:20:39 -0500

    Create Conductores

--- Pages/Conductores/Create.cshtml.cs
+++ Pages/Conductores/Create.cshtml.cs
@@ -1,3 +1,5 @@
+using Flota_Vehicular.Data;
+using Flota_Vehicular.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,31 @@ namespace Flota_Vehicular.Pages.Conductores
 {
     public class CreateModel : PageModel
     {
-        public void OnGet()
-        {
-        }
-    }
+		private readonly FlotavehicularContext _context;
+		public CreateModel(FlotavehicularContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult OnGet()
+		{
+			return Page();
+		}
+
+		[BindProperty]
+		public Conductor Conductor { get; set; } = default!;
+
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid || _context.Conductores == null || Conductor == null)
+			{
+				return Page();
+			}
+
+			_context.Conductores.Add(Conductor);
+			await _context.SaveChangesAsync();
+
+			return RedirectToPage("./Index");
+		}
+	}
 }
