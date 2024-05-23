e6418dc3c93a5d19a90cee0bc29b554bca08d966
JeanRamirez12 <fzafrc@gmail.com>
2024-05-23 15:11:28 -0500

    Index Vehiculo

---
+++ Pages/Vehiculos/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Flota_Vehicular.Pages.Vehiculos
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
