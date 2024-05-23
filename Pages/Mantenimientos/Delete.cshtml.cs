afbc6d2428952f5f739789ba713e1b4275aa83bf
Jhorman montoya <jhormaparaelcreador@gmail.com>
2024-05-23 18:58:15 -0500

    Index Mantenimientos

---
+++ Pages/Mantenimientos/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Flota_Vehicular.Pages.Mantenimientos
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/Mantenimientos/Editcshtml.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Flota_Vehicular.Pages.Mantenimientos
+{
+    public class EditcshtmlModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
