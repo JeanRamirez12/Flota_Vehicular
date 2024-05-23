5674e659035120c3df683591cf833de34bf37721
Jhorman montoya <jhormaparaelcreador@gmail.com>
2024-05-23 15:37:29 -0500

    Index Conductores

---
+++ Pages/Conductores/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Flota_Vehicular.Pages.Conductores
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
